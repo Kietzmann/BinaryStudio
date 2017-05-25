63495bd18862201308032350b32557de546cc9fb
Serhiy Buta <otaman963@gmail.com>
2017-05-25 13:39:35 +0300

    Added GetAnimal method for AnimalProvider that creates new animal depend on typeName

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -5,13 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using ConsoleApplication3.Entity;
 using ConsoleApplication3.Factory;
+using ConsoleApplication3.Provider;
 
 namespace ConsoleApplication3
 {
     class Program
     {
+        private static AnimalProvider Provider { get; set; }
         static void Main(string[] args)
         {
+            Provider = new AnimalProvider();
+
+            
+            //console read animal name
+            //console read animal type
+            try
+            {
+                var animal = Provider.GetAnimal("Barsik", "Tiger");
+                Console.WriteLine(animal.Alias);
+            }
+            catch (ArgumentException e)
+            {
+                //console write wrong name or animal type
+            }
+
+
 //               Controller controller = new Controller();
 //               controller.Changed += controller_Changed;
             Tiger tiger = AbstractAnimalFactory.GetAnimal<Tiger>("Tigr");
--- ConsoleApplication3/Provider/AnimalProvider.cs
+++ ConsoleApplication3/Provider/AnimalProvider.cs
@@ -34,5 +34,36 @@ namespace ConsoleApplication3.Provider
             }
             return result;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="typeName"></param>
+        /// <exception cref="ArgumentException"></exception>
+        internal Animal GetAnimal(string name, string typeName)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("", "name");
+            }
+            try
+            {
+                var animalType = Type.GetType(GetStandartizedNameOfAnimalClass(typeName), true);
+                return (Animal) Activator.CreateInstance(animalType, name);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(typeName, "typeName", e);
+            }
+        }
+
+        private string GetStandartizedNameOfAnimalClass(string typeName)
+        {
+            var animalEntityNamespace = "ConsoleApplication3.Entity";
+            var assemblyName = "ConsoleApplication3";
+
+            return string.Format("{0}.{1},{2}", animalEntityNamespace, typeName, assemblyName);
+        }
     }
 }
